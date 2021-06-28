d50a528976200e9a2f98fd47c9b78e7bdf846e48
Christoph <jagodki.cj@gmail.com>
2021-06-28 19:55:03 +0200

    add the initial xcode-project

---
+++ iOS/src/Malinki/Malinki/MalinkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalinkiApp.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 28.06.21.
+//
+
+import SwiftUI
+
+@main
+struct MalinkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
