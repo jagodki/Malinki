8ca791b46df57d81f9bf41217edfb35056863c25
Christoph <jagodki.cj@gmail.com>
2021-06-30 21:54:53 +0200

    add new files for creating the user interface

---
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiMapControl.swift
@@ -0,0 +1,20 @@
+//
+//  MalinkiMapControl.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 30.06.21.
+//
+
+import SwiftUI
+
+struct MalinkiMapControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MalinkiMapControl_Previews: PreviewProvider {
+    static var previews: some View {
+        MalinkiMapControl()
+    }
+}
---
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiMapView.swift
@@ -0,0 +1,20 @@
+//
+//  MalinkiMapView.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 30.06.21.
+//
+
+import SwiftUI
+
+struct MalinkiMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MalinkiMapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MalinkiMapView()
+    }
+}
