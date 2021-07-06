5ea3f6d53664f17b287456fa0233d0230f4ad201
Christoph <jagodki.cj@gmail.com>
2021-07-06 22:52:49 +0200

    add a skeleton for the MKMapView

--- iOS/src/Malinki/Malinki/User Interface/MalinkiMapView.swift
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiMapView.swift
@@ -6,11 +6,134 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MalinkiMapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct MalinkiMapView: UIViewRepresentable {
+    
+    init() {
+        
     }
+    
+    func makeUIView(context: UIViewRepresentableContext<MalinkiMapView>) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        
+        let zoomRange = MKMapView.CameraZoomRange(minCenterCoordinateDistance: 100000)
+        mapView.setCameraZoomRange(zoomRange, animated: true)
+        
+        //register the annotations
+        //...
+        
+        //configure the map view
+        mapView.showsCompass = false
+        mapView.showsTraffic = false
+        mapView.showsBuildings = false
+        mapView.showsScale = false
+        mapView.showsUserLocation = true
+        mapView.mapType = .mutedStandard
+        
+        let scale = MKScaleView(mapView: mapView)
+        scale.frame.origin = CGPoint(x: 15, y: 55)
+        scale.scaleVisibility = .visible
+        mapView.addSubview(scale)
+        
+        //reposition the compass
+        let compassButton = MKCompassButton(mapView:mapView)
+        compassButton.frame.origin = CGPoint(x: 20, y: 100)
+        compassButton.compassVisibility = .adaptive
+        mapView.addSubview(compassButton)
+        
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MalinkiMapView>) {
+        self.updateAnnotations(from: view)
+        self.updateOverlays(from: view)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    private func updateAnnotations(from mapView: MKMapView) {
+        //remove all annotations from the map
+        mapView.removeAnnotations(mapView.annotations)
+    }
+    
+    private func updateOverlays(from mapView: MKMapView) {
+        //remove all overlays from the map
+        mapView.removeOverlays(mapView.overlays)
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let coordinates = view.annotation?.coordinate else { return }
+        let span = mapView.region.span
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+}
+
+final class Coordinator: NSObject, MKMapViewDelegate {
+    var control: MalinkiMapView
+    
+    init(_ control: MalinkiMapView) {
+        self.control = control
+    }
+    
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        if overlay is MKMultiPolyline {
+            let renderer = MKMultiPolylineRenderer(multiPolyline: overlay as! MKMultiPolyline)
+            switch overlay.title {
+            case "river":
+                renderer.strokeColor = UIColor.blue
+                renderer.lineWidth = 2
+            default:
+                renderer.strokeColor = UIColor.green
+                renderer.lineWidth = 2
+            }
+            
+            return renderer
+            
+        } else if overlay is MKMultiPolygon {
+            let renderer = MKMultiPolygonRenderer(multiPolygon: overlay as! MKMultiPolygon)
+            switch overlay.title {
+            case "countries":
+                renderer.strokeColor = UIColor.red
+                renderer.fillColor = UIColor.red.withAlphaComponent(0.5)
+                renderer.lineWidth = 2
+            default:
+                renderer.strokeColor = UIColor.blue
+                renderer.lineWidth = 2
+            }
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        //focus the map on the selected annotation
+        guard let coordinates = view.annotation?.coordinate else { return }
+        let span = mapView.region.span
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: coordinates.latitude - span.latitudeDelta / 4, longitude: coordinates.longitude), span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+//    func mapView(_ mapView: MKMapView, clusterAnnotationForMemberAnnotations memberAnnotations: [MKAnnotation]) -> MKClusterAnnotation {
+//        
+//    }
 }
 
 struct MalinkiMapView_Previews: PreviewProvider {
