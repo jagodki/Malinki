6646d7ff0be468112cea73529accfd54ec6ac1d1
Christoph <jagodki.cj@gmail.com>
2021-07-12 21:44:40 +0200

    add an empty bottom sheet for iOS

--- iOS/src/Malinki/Malinki/User Interface/MalinkiMap.swift
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiMap.swift
@@ -6,8 +6,12 @@
 //
 
 import SwiftUI
+import BottomSheet
 
 struct MalinkiMap: View {
+    
+    @State private var bottomSheetPosition: BottomSheetPosition = .bottom
+    
     var body: some View {
         GeometryReader { geo in
             
@@ -15,18 +19,25 @@ struct MalinkiMap: View {
                 //the map view
                 MalinkiMapView(scaleXPosition: 75, compassXPosition: 15, scaleCompassYPosition: Int(geo.size.height * 0.075))
                     .edgesIgnoringSafeArea(.all)
+                    .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition,
+                                 options: [.appleScrollBehavior],
+                                 title: "Test",
+                                 content: {Text("Content")})
+                    
                 
-                //controls will overlay the map view
-                VStack {
-                    
-                    //some buttons at the top
-                    HStack {
+                if self.bottomSheetPosition != BottomSheetPosition.top {
+                    //controls will overlay the map view
+                    VStack {
+                        
+                        //some buttons at the top
+                        HStack {
+                            Spacer()
+                            MalinkiButtonGroup()
+                        }
+                        
                         Spacer()
-                        MalinkiButtonGroup()
+                        
                     }
-                    
-                    Spacer()
-                    
                 }
             }
             
