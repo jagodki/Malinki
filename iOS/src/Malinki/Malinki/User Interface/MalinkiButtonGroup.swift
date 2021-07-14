374175f5da9cd7ce0108665f5088e1cd92eec35f
Christoph <jagodki.cj@gmail.com>
2021-07-14 21:07:21 +0200

    rearrange the buttons into a grid layout and integrate them into the bottom sheet

--- iOS/src/Malinki/Malinki/User Interface/MalinkiButtonGroup.swift
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiButtonGroup.swift
@@ -9,39 +9,77 @@ import SwiftUI
 import SwiftUIX
 
 struct MalinkiButtonGroup: View {
+    
+    private var columns: [GridItem] = [GridItem(.flexible()),
+                                       GridItem(.flexible()),
+                                       GridItem(.flexible()),
+                                       GridItem(.flexible())]
+    
     var body: some View {
-        ZStack {
-            //a blur effect for the background
-            VisualEffectBlurView(blurStyle: .prominent)
+        
+        LazyVGrid(columns: self.columns, spacing: 2, pinnedViews: []) {
             
-            VStack {
-                //a button for showing the users position
-                Button(action: {
-                        print("Test Position")
-                    
-                }) {
+            //a button for showing the users position
+            Button(action: {
+                print("Test Position")
+                
+            }) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.systemGray5)
                     Image(systemName: "location")
                         .font(.system(size: 20, weight: .regular))
                         .padding()
                 }
+            }
+            .cornerRadius(10)
+            
+            //a button for sharing the map
+            Button(action: {
+                print("Test Share")
                 
-                Divider()
-                
-                //a button for sharing the map
-                Button(action: {
-                        print("Test Position")
-                    
-                }) {
+            }) {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(Color.systemGray5)
                     Image(systemName: "square.and.arrow.up")
                         .font(.system(size: 20, weight: .regular))
                         .padding()
                 }
             }
+            .cornerRadius(10)
+            
+            //a button for toggling the compass
+                Button(action: {
+                    print("Test Compass")
+
+                }) {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(Color.systemGray5)
+                        Image(systemName: "square.and.arrow.up")
+                            .font(.system(size: 20, weight: .regular))
+                            .padding()
+                    }
+                }
+                .cornerRadius(10)
+
+                //a button for toggling the scale
+                Button(action: {
+                    print("Test Scale")
+
+                }) {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(Color.systemGray5)
+                        Image(systemName: "square.and.arrow.up")
+                            .font(.system(size: 20, weight: .regular))
+                            .padding()
+                    }
+                }
+                .cornerRadius(10)
         }
-        .frame(width: 50, height: 130)
-        .cornerRadius(10)
-        .shadow(radius: 2.5)
-        .padding()
+        
     }
 }
 
--- iOS/src/Malinki/Malinki/User Interface/MalinkiMap.swift
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiMap.swift
@@ -14,33 +14,20 @@ struct MalinkiMap: View {
     
     var body: some View {
         GeometryReader { geo in
-            
-            ZStack {
-                //the map view
-                MalinkiMapView(scaleXPosition: 75, compassXPosition: 15, scaleCompassYPosition: Int(geo.size.height * 0.075))
-                    .edgesIgnoringSafeArea(.all)
-                    .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition,
-                                 options: [.appleScrollBehavior],
-                                 title: "Test",
-                                 content: {Text("Content")})
-                    
-                
-                if self.bottomSheetPosition != BottomSheetPosition.top {
-                    //controls will overlay the map view
-                    VStack {
-                        
-                        //some buttons at the top
-                        HStack {
-                            Spacer()
-                            MalinkiButtonGroup()
-                        }
-                        
-                        Spacer()
-                        
-                    }
-                }
-            }
-            
+            //the map view
+            MalinkiMapView(scaleXPosition: 75, compassXPosition: 15, scaleCompassYPosition: Int(geo.size.height * 0.075))
+                .edgesIgnoringSafeArea(.all)
+                .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition,
+                             options: [.appleScrollBehavior],
+                             title: "Test",
+                             content: {
+                                VStack {
+                                    Text("Content")
+                                    MalinkiButtonGroup()
+                                        .padding()
+                                }
+                                
+                             })
         }
         
     }
