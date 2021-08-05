5e72f0673717824c3551c14038a5c394a588c298
Christoph <jagodki.cj@gmail.com>
2021-08-05 21:24:29 +0200

    add documentation to MalinkiBasemapButton

--- iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiBasemapButton.swift
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiBasemapButton.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
+/// A structure to create a Basemap Button.
 struct MalinkiBasemapButton: View {
     
     @Binding private var isToggled: Bool
-    private var imagePath: String
+    private var imageName: String
     private var basemapName: String
     
-    init(isToggled: Binding<Bool>, imagePath: String, basemapName: String) {
+    /// The initialiser of this structure.
+    /// - Parameters:
+    ///   - isToggled: a binding indicating, if the button is toggled or not
+    ///   - imageName: the name of the image
+    ///   - basemapName: the name of the basemap, presented at the bottom of the button
+    init(isToggled: Binding<Bool>, imageName: String, basemapName: String) {
         self._isToggled = isToggled
-        self.imagePath = imagePath
+        self.imageName = imageName
         self.basemapName = basemapName
     }
     
@@ -26,7 +32,7 @@ struct MalinkiBasemapButton: View {
                 print("Theme Button pressed")
             }) {
                 VStack {
-                    Image(self.imagePath)
+                    Image(self.imageName)
                         .frame(minWidth: 0, idealWidth: 100, maxWidth: 100, minHeight: 50, idealHeight: 50, maxHeight: 50, alignment: .center)
                         .cornerRadius(5)
                         .overlay(RoundedRectangle(cornerRadius: 5)
@@ -45,6 +51,6 @@ struct MalinkiBasemapButton: View {
 
 struct MalinkiBasemapButton_Previews: PreviewProvider {
     static var previews: some View {
-        MalinkiBasemapButton(isToggled: .constant(true), imagePath: "osm_basemap", basemapName: "OSM Street")
+        MalinkiBasemapButton(isToggled: .constant(true), imageName: "osm_basemap", basemapName: "OSM Street")
     }
 }
