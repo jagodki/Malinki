edb991811d8e433ab78103e9a9405569fec37e6c
Christoph <jagodki.cj@gmail.com>
2021-08-05 21:19:58 +0200

    add documentation to the map theme buttons

--- iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapThemeButton.swift
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapThemeButton.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+/// A structure to create a Map Theme Button.
 struct MalinkiMapThemeButton: View {
     
     @Binding private var isToggled: Bool
@@ -15,6 +16,13 @@ struct MalinkiMapThemeButton: View {
     private var secondColour: Color
     private var themeName: String
     
+    /// The initialiser of this structure
+    /// - Parameters:
+    ///   - imageName: the system name of the image
+    ///   - firstColour: the primary colour of the button, used for the background if toggled
+    ///   - secondColour: the secondary colour, used for the image if toggled
+    ///   - isThemeToggled: a binding indicating, if the button is toggled or not
+    ///   - themeName: the name of the theme, presented at the bottom of the button
     init(imageName: String, firstColour: Color, secondColour: Color, isThemeToggled: Binding<Bool>, themeName: String) {
         self.imageName = imageName
         self.firstColour = firstColour
@@ -41,7 +49,7 @@ struct MalinkiMapThemeButton: View {
             Text(self.themeName)
                 .frame(width: 100, height: 20, alignment: .center)
                 .font(.footnote)
-                .foregroundColor(.secondary)
+                .foregroundColor(.primary)
         }
     }
 }
