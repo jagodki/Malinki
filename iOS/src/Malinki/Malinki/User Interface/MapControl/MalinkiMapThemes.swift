cb40b2d0bc21b609957b31a344a7d371c49c121f
Christoph <jagodki.cj@gmail.com>
2021-08-11 21:14:30 +0200

    Create MalinkiMapThemes.swift

---
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapThemes.swift
@@ -0,0 +1,20 @@
+//
+//  MalinkiMapThemes.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 11.08.21.
+//
+
+import SwiftUI
+
+struct MalinkiMapThemes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MalinkiMapThemes_Previews: PreviewProvider {
+    static var previews: some View {
+        MalinkiMapThemes()
+    }
+}
