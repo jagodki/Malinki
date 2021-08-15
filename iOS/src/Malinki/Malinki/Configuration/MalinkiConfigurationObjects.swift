20b96734776d60329f1682a283d4c777a8850e5a
Christoph <jagodki.cj@gmail.com>
2021-08-15 19:43:13 +0200

    create first objects representing the configuration

---
+++ iOS/src/Malinki/Malinki/Configuration/MalinkiConfigurationObjects.swift
@@ -0,0 +1,31 @@
+//
+//  MalinkiConfigurationObjects.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 15.08.21.
+//
+
+import Foundation
+
+struct MalinkiConfiguration: Decodable {
+    var mapThemes: [MalinkiConfigurationTheme]
+    var basemaps: [MalinkiConfigurationBasemap]
+}
+
+struct MalinkiConfigurationTheme: Decodable {
+    var test: String
+}
+
+struct MalinkiConfigurationBasemap: Decodable {
+    var id: Int
+    var internalName: String
+    var externalName: MalinkiConfigurationExternalName
+}
+
+struct MalinkiConfigurationExternalName: Decodable {
+    var en: String
+    var de: String
+    var fr: String
+    var sv: String
+    var pl: String
+}
