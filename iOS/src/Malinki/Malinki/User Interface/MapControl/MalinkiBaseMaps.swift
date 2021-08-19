37e7b703f77ff21e6889517669b1bcc34b84e640
Christoph <jagodki.cj@gmail.com>
2021-08-19 20:18:12 +0200

    remove some placeholders in the basemaps

--- iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiBaseMaps.swift
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiBaseMaps.swift
@@ -31,68 +31,9 @@ struct MalinkiBasemaps: View {
         LazyVGrid(columns: self.columns, spacing: 2, pinnedViews: []) {
             
             ForEach(MalinkiConfigurationProvider.sharedInstance.configData?.basemaps ?? [], id: \.id) { basemap in
-                MalinkiBasemapButton(isToggled: .constant(false), imageName: basemap.imageName, basemapName: basemap.externalNames.de)
+                MalinkiBasemapButton(isToggled: .constant(basemap.onStartUp), imageName: basemap.imageName, basemapName: basemap.externalNames.de)
             }
             
-//            //a button for showing the users position
-//            Button(action: {
-//                print("Test Position")
-//
-//            }) {
-//                ZStack {
-//                    Rectangle()
-//                        .foregroundColor(Color(UIColor.systemGray5))
-//                    Image(systemName: "location")
-//                        .font(.system(size: 20, weight: .regular))
-//                        .padding()
-//                }
-//            }
-//            .cornerRadius(10)
-//
-//            //a button for sharing the map
-//            Button(action: {
-//                print("Test Share")
-//
-//            }) {
-//                ZStack {
-//                    Rectangle()
-//                        .foregroundColor(Color(UIColor.systemGray5))
-//                    Image(systemName: "square.and.arrow.up")
-//                        .font(.system(size: 20, weight: .regular))
-//                        .padding()
-//                }
-//            }
-//            .cornerRadius(10)
-//
-//            //a button for toggling the compass
-//                Button(action: {
-//                    print("Test Compass")
-//
-//                }) {
-//                    ZStack {
-//                        Rectangle()
-//                            .foregroundColor(Color(UIColor.systemGray5))
-//                        Image(systemName: "square.and.arrow.up")
-//                            .font(.system(size: 20, weight: .regular))
-//                            .padding()
-//                    }
-//                }
-//                .cornerRadius(10)
-//
-//                //a button for toggling the scale
-//                Button(action: {
-//                    print("Test Scale")
-//
-//                }) {
-//                    ZStack {
-//                        Rectangle()
-//                            .foregroundColor(Color(UIColor.systemGray5))
-//                        Image(systemName: "square.and.arrow.up")
-//                            .font(.system(size: 20, weight: .regular))
-//                            .padding()
-//                    }
-//                }
-//                .cornerRadius(10)
         }
         
     }
