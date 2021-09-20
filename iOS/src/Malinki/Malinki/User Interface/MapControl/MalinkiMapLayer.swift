ced4423df27a38bf04dbf38872ecb783fca28be0
Christoph <jagodki.cj@gmail.com>
2021-09-20 21:14:06 +0200

    remove state in maplayer, add documentation

--- iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapLayer.swift
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapLayer.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 /// A structure to descripe a map layer.
 struct MalinkiMapLayer {
+    
     let id: Int
     let name: String
     let image: Image
-    @State var isToggled: Bool = true
+    var isToggled: Bool = true
     
+    /// The initialiser of this structure.
+    /// - Parameters:
+    ///   - id: the ID of the map layer
+    ///   - name: the external name of the map layer
+    ///   - imageName: the image name of the map layer
     init(id: Int, name: String, imageName: String) {
         self.id = id
         self.name = name
