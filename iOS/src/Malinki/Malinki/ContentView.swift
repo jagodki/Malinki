8d85f960f78a4f6ed0a6d03d193e4f7e77dd8707
Christoph <jagodki.cj@gmail.com>
2021-10-19 21:37:20 +0200

    available if iOS 15 or newer because of adaptive sheet

--- iOS/src/Malinki/Malinki/ContentView.swift
+++ iOS/src/Malinki/Malinki/ContentView.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 struct ContentView: View {
     var body: some View {
         MalinkiMap()
     }
 }
 
+@available(iOS 15.0, *)
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- iOS/src/Malinki/Malinki/MalinkiApp.swift
+++ iOS/src/Malinki/Malinki/MalinkiApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@available(iOS 15.0, *)
 @main
 struct MalinkiApp: App {
     var body: some Scene {
