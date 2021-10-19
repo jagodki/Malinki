68c29b0cb57429f4de557b78c311770dc2b4e80a
Christoph <jagodki.cj@gmail.com>
2021-10-19 21:36:17 +0200

    Update MalinkiMapThemes.swift

--- iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapThemes.swift
+++ iOS/src/Malinki/Malinki/User Interface/MapControl/MalinkiMapThemes.swift
@@ -11,10 +11,12 @@ struct MalinkiMapThemes: View {
     
     @Binding private var mapThemeID: Int
     @Binding private var mapLayers: [MalinkiMapLayer]
+    @Binding private var showBasemapsSheet: Bool
     
-    init(mapThemeID: Binding<Int>, mapLayers: Binding<[MalinkiMapLayer]>) {
+    init(mapThemeID: Binding<Int>, mapLayers: Binding<[MalinkiMapLayer]>, showBasemapsSheet: Binding<Bool>) {
         self._mapThemeID = mapThemeID
         self._mapLayers = mapLayers
+        self._showBasemapsSheet = showBasemapsSheet
     }
     
 //    var columns: [GridItem] =
@@ -25,16 +27,28 @@ struct MalinkiMapThemes: View {
         Menu {
             Picker(selection: self.$mapThemeID, label: Text("Map Themes")) {
                 ForEach(MalinkiConfigurationProvider.sharedInstance.getMapThemes(), id: \.id) { mapTheme in
-                    Button(action: {}) {
+                    Button(action: {
+                        self.mapThemeID = mapTheme.id
+                    }) {
                         Text(mapTheme.externalNames.en)
                         Image(systemName: mapTheme.iconName)
                     }.tag(mapTheme.id)
                 }
             }
+            
+            Button(action: {
+                self.showBasemapsSheet = true
+            }) {
+                Text(LocalizedStringKey("Basemaps"))
+                Image(systemName: "square.2.stack.3d.bottom.filled")
+            }
         } label: {
             Image(systemName: "map.fill")
+                .padding(.all, 10.0)
                 .foregroundColor(Color.primary)
+                .background(Color(UIColor.systemGray3).opacity(0.75))
                 .font(.title)
+                .cornerRadius(10)
         }
         
 //        ScrollView(.horizontal) {
@@ -50,6 +64,6 @@ struct MalinkiMapThemes: View {
 
 struct MalinkiMapThemes_Previews: PreviewProvider {
     static var previews: some View {
-        MalinkiMapThemes(mapThemeID: .constant(0), mapLayers: .constant([]))
+        MalinkiMapThemes(mapThemeID: .constant(0), mapLayers: .constant([]), showBasemapsSheet: .constant(false))
     }
 }
