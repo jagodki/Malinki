bf1619ee6d0b473cc186e7067227005aead3a72c
Christoph <jagodki.cj@gmail.com>
2021-11-03 21:05:11 +0100

    Update MalinkiSheet.swift

--- iOS/src/Malinki/Malinki/User Interface/MalinkiSheet.swift
+++ iOS/src/Malinki/Malinki/User Interface/MalinkiSheet.swift
@@ -1,18 +1,24 @@
 //
-//  MalinkiSheetType.swift
+//  MalinkiState.swift
 //  Malinki
 //
-//  Created by Christoph Jung on 31.10.21.
+//  Created by Christoph Jung on 02.11.21.
 //
 
 import Foundation
+import Combine
 
-class MalinkiSheet: MalinkiSheetState<MalinkiSheet.State> {
-    
-    enum State {
-        case basemaps
-        case layers
-        case details
+class MalinkiSheet: ObservableObject {
+    @Published var isShowing: Bool = false
+    @Published var state: MalinkiSheetState? = nil {
+        didSet {
+            self.isShowing = (state != nil)
+        }
     }
-    
+}
+
+enum MalinkiSheetState {
+    case basemaps
+    case layers
+    case details
 }
