49e907deb025aaa11d38d29cc2e1b419f6dbcde2
Christoph <jagodki.cj@gmail.com>
2021-11-29 21:38:58 +0100

    add the corresponding raster layer id to the vector layer config

--- iOS/src/Malinki/Malinki/Configuration/MalinkiConfigurationObjects.swift
+++ iOS/src/Malinki/Malinki/Configuration/MalinkiConfigurationObjects.swift
@@ -42,6 +42,7 @@ struct MalinkiConfigurationVectorData: Decodable {
     var id: Int
     var internalName: String
     var externalNames: MalinkiConfigurationBasemapExternalName
+    var correspondingRasterLayer: Int
     var vectorTypes: MalinkiConfigurationVectorTypes
     var attributes: MalinkiConfigurationVectorAttributes
     var featureInfo: MalinkiConfigurationVectorFeatureInfo
