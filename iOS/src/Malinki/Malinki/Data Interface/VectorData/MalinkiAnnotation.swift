bc6ed3eb2382a659e982a1f48ff202febb526834
Christoph <jagodki.cj@gmail.com>
2021-12-01 22:32:20 +0100

    new attributes for annotations

--- iOS/src/Malinki/Malinki/Data Interface/VectorData/MalinkiAnnotation.swift
+++ iOS/src/Malinki/Malinki/Data Interface/VectorData/MalinkiAnnotation.swift
@@ -14,11 +14,17 @@ class MalinkiAnnotation: NSObject, MKAnnotation, ObservableObject {
     var title: String?
     var subtitle: String?
     var coordinate: CLLocationCoordinate2D
+    var themeID: Int
+    var layerID: Int
+    var featureID: String
     
-    init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D) {
+    init(title: String?, subtitle: String?, coordinate: CLLocationCoordinate2D, themeID: Int, layerID: Int, featureID: String) {
         self.title = title
         self.subtitle = subtitle
         self.coordinate = coordinate
+        self.themeID = themeID
+        self.layerID = layerID
+        self.featureID = featureID
     }
     
 }
