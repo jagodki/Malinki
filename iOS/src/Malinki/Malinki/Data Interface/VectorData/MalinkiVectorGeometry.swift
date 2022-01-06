93dfb8befe0105d876e5d272edad150e7ba38d29
Christoph <jagodki.cj@gmail.com>
2022-01-06 21:01:52 +0100

    new class to provide vector geometry types from MKShape

---
+++ iOS/src/Malinki/Malinki/Data Interface/VectorData/MalinkiVectorGeometry.swift
@@ -0,0 +1,57 @@
+//
+//  MalinkiVectorGeometry.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 17.11.21.
+//
+
+import Foundation
+import MapKit
+
+public class MalinkiVectorGeometry {
+    
+    enum GeometryType {
+        case point
+        case linestring
+        case polygon
+        case multilinestring
+        case multipolygon
+        case non
+    }
+    
+    let type: GeometryType
+    var mapThemeID: Int
+    var layerID: Int
+    var point: MKPointAnnotation = MKPointAnnotation()
+    var linestring: MKPolyline = MKPolyline()
+    var multiLinestring: MKMultiPolyline = MKMultiPolyline()
+    var polygon: MKPolygon = MKPolygon()
+    var multiPolygon: MKMultiPolygon = MKMultiPolygon()
+    
+    init(mapThemeID: Int, layerID: Int, geometry: MKShape) {
+        self.mapThemeID = mapThemeID
+        self.layerID = layerID
+        
+        //get the geometry type and the geometry itself
+        if geometry is MKPointAnnotation {
+            self.type = GeometryType.point
+            self.point = (geometry as? MKPointAnnotation)!
+        } else if geometry is MKPolygon {
+            self.type = GeometryType.polygon
+            self.polygon = (geometry as? MKPolygon)!
+        } else if geometry is MKMultiPolygon {
+            self.type = GeometryType.multipolygon
+            self.multiPolygon = (geometry as? MKMultiPolygon)!
+        } else if geometry is MKPolyline {
+            self.type = GeometryType.linestring
+            self.linestring = (geometry as? MKPolyline)!
+        } else if geometry is MKMultiPolyline {
+            self.type = GeometryType.multilinestring
+            self.multiLinestring = (geometry as? MKMultiPolyline)!
+        } else {
+            self.type = GeometryType.non
+            print("geometry error or no geometry")
+        }
+    }
+    
+}
