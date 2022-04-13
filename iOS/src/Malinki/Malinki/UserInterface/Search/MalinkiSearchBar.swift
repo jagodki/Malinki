7f365cebb2dc83e66c561f56c56d120f1ec0605f
Christoph <jagodki.cj@gmail.com>
2022-04-13 22:39:02 +0200

    adjust sheet detent when editing started

--- iOS/src/Malinki/Malinki/UserInterface/Search/MalinkiSearchBar.swift
+++ iOS/src/Malinki/Malinki/UserInterface/Search/MalinkiSearchBar.swift
@@ -8,26 +8,29 @@
 import SwiftUI
 
 /// A structure to present a SearchBar written in pure SwiftUI for integration into a BottomSheet.
+@available(iOS 15.0, *)
 struct MalinkiSearchBar: View {
     
     @Binding private var searchText: String
     @Binding private var isEditing: Bool
+    @Binding private var sheetDetent: UISheetPresentationController.Detent.Identifier?
     
     /// The initialiser of the struct.
     /// - Parameters:
     ///   - bottomSheetPosition: a binding to the position of the BottomSheet where the SearchBar is located in
     ///   - searchText: a binding to the text presented in the SearchBar
     ///   - isEditing: a binding to a Bool, if the SearchBar is in editing mode
-    init(searchText: Binding<String>, isEditing: Binding<Bool>) {
+    init(searchText: Binding<String>, isEditing: Binding<Bool>, sheetDetent: Binding<UISheetPresentationController.Detent.Identifier?>) {
         self._searchText = searchText
         self._isEditing = isEditing
+        self._sheetDetent = sheetDetent
     }
     
     var body: some View {
         HStack {
             HStack {
                 Image(systemName: "magnifyingglass")
-                TextField("Search", text: self.$searchText).foregroundColor(.primary)
+                TextField(LocalizedStringKey("Search for Map Themes and Content"), text: self.$searchText).foregroundColor(.primary)
 
                 if self.searchText != "" {
                     Button(action: {
@@ -47,6 +50,7 @@ struct MalinkiSearchBar: View {
             //When you tap the SearchBar, the BottomSheet moves to the .top position to make room for the keyboard.
             .onTapGesture {
                 self.isEditing = true
+                self.sheetDetent = .large
             }
 
             if self.isEditing {
@@ -56,8 +60,9 @@ struct MalinkiSearchBar: View {
                     UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
 
                 }) {
-                    Text("Cancel")
+                    Text(LocalizedStringKey("Cancel"))
                 }
+                .foregroundColor(.accentColor)
                 .padding(.horizontal, 5)
                 .padding(.bottom)
                 .transition(.move(edge: .trailing))
@@ -67,8 +72,9 @@ struct MalinkiSearchBar: View {
     }
 }
 
+@available(iOS 15.0, *)
 struct MalinkiSearchBar_Previews: PreviewProvider {
     static var previews: some View {
-        MalinkiSearchBar(searchText: .constant(""), isEditing: .constant(true))
+        MalinkiSearchBar(searchText: .constant(""), isEditing: .constant(true), sheetDetent: .constant(UISheetPresentationController.Detent.Identifier.large))
     }
 }
