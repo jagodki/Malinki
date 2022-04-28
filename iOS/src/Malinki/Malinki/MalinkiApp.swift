ea3f128c315f1166b55c1e1950352cd1a9b2fa34
Christoph <jagodki.cj@gmail.com>
2022-04-28 21:30:23 +0200

    remove cache directory when app enters background

--- iOS/src/Malinki/Malinki/MalinkiApp.swift
+++ iOS/src/Malinki/Malinki/MalinkiApp.swift
@@ -10,6 +10,26 @@ import SwiftUI
 @available(iOS 15.0, *)
 @main
 struct MalinkiApp: App {
+    
+    init() {
+        NotificationCenter.default.addObserver(forName: UIApplication.didEnterBackgroundNotification, object: nil, queue: .main, using: {_ in
+            
+            //get the path
+            let fileManager = FileManager.default
+            let documentsUrl =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            let cachePath = documentsUrl?.path.stringByAppendingPathComponent(path: "TILE_CACHE")
+            
+            //clear cache
+            do {
+                if let mapCachePath = cachePath {
+                    try fileManager.removeItem(atPath: mapCachePath)
+                }
+            } catch {
+                print("ERROR - could not clear cache folder: \(error)")
+            }
+        })
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
