a7222871f38a556b06f4fbf7ff9487a75d4747a1
Christoph <jagodki.cj@gmail.com>
2022-05-03 21:09:25 +0200

    Create MalinkiBookmarksObjects.swift

---
+++ iOS/src/Malinki/Malinki/Data Interface/Bookmarks/MalinkiBookmarksObjects.swift
@@ -0,0 +1,37 @@
+//
+//  MalinkiBookmarksObjects.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 03.05.22.
+//
+
+import Foundation
+
+//MARK: - Root Level
+struct MalinkiBookmarksRoot: Codable {
+    var bookmarks: [MalinkiBookmarksObject]
+}
+
+//MARK: - bookmark object
+struct MalinkiBookmarksObject: Codable {
+    var name: String
+    var colour: String
+    var theme_id: Int
+    var layer_ids: [Int]
+}
+
+//MARK: - map component of bookmarks
+struct MalinkiBookmarksMap: Codable {
+    var centre: MalinkiBookmarksMapCentre
+    var span: MalinkiBookmarksMapSpan
+}
+
+struct MalinkiBookmarksMapCentre: Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct MalinkiBookmarksMapSpan: Codable {
+    var delta_latitude: Double
+    var delta_longitude: Double
+}
