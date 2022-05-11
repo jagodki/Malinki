a7588740fcce00371f2e8114ce5a0b19701088cb
Christoph <jagodki.cj@gmail.com>
2022-05-11 22:49:37 +0200

    Create MalinkiBookmarksView.swift

---
+++ iOS/src/Malinki/Malinki/UserInterface/Bookmarks/MalinkiBookmarksView.swift
@@ -0,0 +1,20 @@
+//
+//  MalinkiBookmarksView.swift
+//  Malinki
+//
+//  Created by Christoph Jung on 11.05.22.
+//
+
+import SwiftUI
+
+struct MalinkiBookmarksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MalinkiBookmarksView_Previews: PreviewProvider {
+    static var previews: some View {
+        MalinkiBookmarksView()
+    }
+}
