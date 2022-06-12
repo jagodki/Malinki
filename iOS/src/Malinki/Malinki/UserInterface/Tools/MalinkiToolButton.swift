b322609b83cdee0523fb55fb486dd392e5a3fce6
Christoph <jagodki.cj@gmail.com>
2022-06-12 20:38:25 +0200

    add documentation

--- iOS/src/Malinki/Malinki/UserInterface/Tools/MalinkiToolButton.swift
+++ iOS/src/Malinki/Malinki/UserInterface/Tools/MalinkiToolButton.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+/// A structur to show a menu button with several tools available.
 struct MalinkiToolButton: View {
     
     @Binding var sheetState: MalinkiSheetState?
     
     var body: some View {
         Menu {
-            
+            //the search entry
             Button(action: {
                 self.sheetState = .search
             }) {
@@ -21,12 +22,15 @@ struct MalinkiToolButton: View {
                 Image(systemName: "magnifyingglass")
             }
             
+            //the bookmarks entry
             Button(action: {
                 self.sheetState = .bookmarks
             }) {
                 Text(LocalizedStringKey("Bookmarks"))
                 Image(systemName: "bookmark.fill")
             }
+            
+            //the clean cache entry
         } label: {
             Image(systemName: "ellipsis")
                 .font(.title2)
